6c85dbe02c035d414fdd555be85a8c3102122303
manuel <manuelturlan14@gmail.com>
2022-07-02 20:18:20 -0400

    Ejercicio 3 terminado

---
+++ factorial-for.js
@@ -0,0 +1,7 @@
+let num = 10;
+
+for (let i = num-1; i > 0; i--) {
+    num = num * i;
+}
+
+console.log(num);
---
+++ factorial-while.js
@@ -0,0 +1,11 @@
+let num = 10;
+let aux = 9;
+let i = 0
+
+while (i < 9) {
+    num = num * aux;
+    aux--
+    i++
+}
+
+console.log(num)
